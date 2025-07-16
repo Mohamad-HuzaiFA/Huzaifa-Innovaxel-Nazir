5a71d96b19aeb4ddd6582f7f89b391d7c7d7a934
Mohamad-HuzaiFA <huzaifanazir143@gmail.com>
2025-07-16 16:11:12 +0500

    feat: Update and Delete tested add added

--- url-shortner/shortner/urls.py
+++ url-shortner/shortner/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('shorten', views.create_short_url, name='create_short_url'),
     
     path('shorten/<str:short_code>', views.retrieve_original_url, name='retrieve_url'),
+    path('shorten/<str:short_code>/update', views.update_short_url, name='update_url'),
+    path('shorten/<str:short_code>/delete', views.delete_short_url, name='delete_url'),
 ]
--- url-shortner/shortner/views.py
+++ url-shortner/shortner/views.py
@@ -22,3 +22,19 @@ def retrieve_original_url(request, short_code):
     short_url.save()
     # return Response(ShortURLSerializer(short_url).data) This will return JSON object by whiich we can manually redierect by extrecting thr url
     return HttpResponseRedirect(short_url.url) #this will directly redirect to the original url
+
+
+@api_view(['PUT'])
+def update_short_url(request, short_code):
+    short_url = get_object_or_404(ShortURL, shortCode=short_code)
+    serializer = ShortURLSerializer(short_url, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_short_url(request, short_code):
+    short_url = get_object_or_404(ShortURL, shortCode=short_code)
+    short_url.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)

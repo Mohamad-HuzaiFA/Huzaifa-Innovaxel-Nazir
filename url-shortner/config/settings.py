23078f14486cc2d6f8129769833dc8bea3e5b2dc
Mohamad-HuzaiFA <huzaifanazir143@gmail.com>
2025-07-16 17:08:00 +0500

    fix: using Pymysql instead of mysqlclient

--- url-shortner/config/settings.py
+++ url-shortner/config/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for config project.
 
@@ -9,8 +10,10 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import pymysql
+pymysql.install_as_MySQLdb()
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent

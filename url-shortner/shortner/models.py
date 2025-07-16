afd171302407645d007d6dde54c454660cc50868
Mohamad-HuzaiFA <huzaifanazir143@gmail.com>
2025-07-16 15:35:35 +0500

    Added ShortURL model

--- url-shortner/shortner/models.py
+++ url-shortner/shortner/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import string, random
 
-# Create your models here.
+def generate_shortcode():
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+
+class ShortURL(models.Model):
+    url = models.URLField()
+    shortCode = models.CharField(max_length=6, unique=True, default=generate_shortcode)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    accessCount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.shortCode} -> {self.url}"

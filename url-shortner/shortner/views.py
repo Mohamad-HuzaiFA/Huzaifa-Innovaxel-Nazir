c88b63d695fe7f230e3375a32e6a18cdb9a85445
Mohamad-HuzaiFA <huzaifanazir143@gmail.com>
2025-07-16 16:02:34 +0500

    Update: redirection to original url on retreival

--- url-shortner/shortner/views.py
+++ url-shortner/shortner/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from .models import ShortURL
 from .serializers import ShortURLSerializer
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect # fro redirection
 
 @api_view(['POST'])
 def create_short_url(request):
@@ -19,4 +20,5 @@ def retrieve_original_url(request, short_code):
     short_url = get_object_or_404(ShortURL, shortCode=short_code)
     short_url.accessCount += 1
     short_url.save()
-    return Response(ShortURLSerializer(short_url).data) 
+    # return Response(ShortURLSerializer(short_url).data) This will return JSON object by whiich we can manually redierect by extrecting thr url
+    return HttpResponseRedirect(short_url.url) #this will directly redirect to the original url
